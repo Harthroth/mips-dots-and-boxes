950b165c5cca378b30ffefde5926420d5e74a5e9
George-Zhang5246 <70183314+George-Zhang5246@users.noreply.github.com>
2023-04-27 19:14:39 -0500

    Update computerTurn.asm

--- computerTurn.asm
+++ computerTurn.asm
@@ -26,37 +26,54 @@ columnLoop:
 	sll $t3, $t0, 3
 	add $t3, $t3, $t1
 	lbu $t4, horizontalLineArray($t3)	# top line must exist
-	bne $t4, 1, Continue	# if horiArr[i*8 + j] != 1, branch
+	add $t6, $t6, $t4        # add horiArr(t3) to t6
 	
 	# t3 = 8*(t0+1) + t1
 	addi $t3, $t3, 8
 	lbu $t4, horizontalLineArray($t3)	# bottom line must exist
-	bne $t4, 1, Continue	# if horiArr[(i+1)*8 + j] != 1, branch
+	add $t6, $t6, $t4	# add horiArr(t3) to t6
 	
 	# t3 = 9*t0 + j
 	mul $t3, $t0, 9
 	add $t3, $t3, $t1
 	lbu $t4, verticalLineArray($t3)	# left line must exist
-	bne $t4, 1, Continue	# if vertArr[i*9 + j] != 1, branch
+	add $t6, $t6, $t4	# add vertArr(t3) to t6
 	
 	# t3 = 9*t0 + j + 1
 	addi $t3, $t3, 1
 	lbu $t4, verticalLineArray($t3)	# right line must exist
-	bne $t4, 1, Continue	# if vertArr[i*9 + j + 1] != 1, branch
-	
-	# t3 = 8*t0 + j
-	sll $t3, $t0, 3
-	add $t3, $t3, $t1
-	lbu $t4, boxArray($t3)	# box cannot already exist
-	bne $t4, 0, Continue	# if box[i*8 + j] != 0, branch
+	add $t6, $t6, $t4	# add vertArr(t3) to t6
 	
 	# store the turn number of the player who just completed a box in index t3 of boxArray
 	# the turn number is passed in to the subroutine in register $a3
 	
-	sb $a0, boxArray($t3)
-	addi $v0, $v0, 1 # increment return value v0 by 1 to indicate that player completed a box
+	bne $t6, 3, Continue		# if there are not 3 sides in the box, continue
 	
-	beq $v0, 2, outerLoopEnd	# if v0 is set to 2, maximum number of new boxes have been completed, no need to keep searching
+	# find line which is not filled, fill the line
+	# t3 = 8*t0 + t1
+	sll $t3, $t0, 3
+	add $t3, $t3, $t1
+	lbu $t4, horizontalLineArray($t3)	# top line must exist
+	bne $t4, 0, return	# if horiArr[i*8 + j] != 1, branch
+	
+	# t3 = 8*(t0+1) + t1
+	addi $t3, $t3, 8
+	lbu $t4, horizontalLineArray($t3)	# bottom line must exist
+	bne $t4, 1, return	# if horiArr[(i+1)*8 + j] != 1, branch
+	
+	# t3 = 9*t0 + t1
+	mul $t3, $t0, 9
+	add $t3, $t3, $t1
+	lbu $t4, verticalLineArray($t3)	# left line must exist
+	bne $t4, 1, return	# if vertArr[i*9 + j] != 1, branch
+	
+	# t3 = 9*t0 + t1 + 1
+	addi $t3, $t3, 1
+	lbu $t4, verticalLineArray($t3)	# right line must exist
+	bne $t4, 1, return	# if vertArr[i*9 + j + 1] != 1, branch
+	
+	return:
+	
 
 Continue:
 	addi $t1, $t1, 1	# increment j
@@ -66,6 +83,10 @@ innerLoopEnd:
 	addi $t0, $t0, 1	# increment i
 	move $t1, $zero	# reset j
 	j rowLoop
+
+outerLoopEnd:
+	j generateRandom
+
 
 # generate a random value if there is no box to complete
 generateRandom:	
@@ -114,4 +135,8 @@ generateRandom:
     	beq $s0, 1, E
     	beq $s0, 2, S
     	beq $s0, 3, W
+	
+	j $ra	#return
+	
+	
 
